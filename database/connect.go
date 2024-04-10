b089bf43c99c60aa98514814072319d0c86f3d2b
ghirmayH <161266379+ghirmayH@users.noreply.github.com>
2024-04-09 22:16:21 -0400

    Add files via upload

---
+++ database/connect.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	// Connect to database here
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dsn := os.Getenv("DSN")
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Couldnt connect to database")
+	}
+	DB = database
+}
